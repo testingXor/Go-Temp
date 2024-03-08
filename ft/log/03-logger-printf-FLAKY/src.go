d0fb73ea6f717e14af9f1fa6c9988a0d8a4057f3
testingXor <testing@openrefactory.com>
2024-03-08 06:10:08 +0000

    Fix Prevent Sensitive Data Leakage
    In file: src.go, there is a function handler that is leaking sensitive data through logging. iCR suggested proper sanitization to prevent this sensitive data leakage.

--- ft/log/03-logger-printf-FLAKY/src.go
+++ ft/log/03-logger-printf-FLAKY/src.go
@@ -18,5 +18,14 @@ func handler(req *http.Request) {
 	username := req.URL.Query().Get("username")
 	password := req.URL.Query().Get("password")
 	logger := log.New(os.Stdout, "", log.LstdFlags)
+	// OpenRefactory Warning:
+	// Possible Sensitive Data Leak!
+	// Path:
+	//	File: src.go, Line: 19
+	//		password := req.URL.Query().Get("password")
+	//		Variable 'password' is assigned a tainted value.
+	//	File: src.go, Line: 21
+	//		logger.Printf("User %s logged in with password %s", username, password)
+	//		Tainted information is used in a sink.
 	logger.Printf("User %s logged in with password %s", username, password)
 }
